ea0971ceff4eb955f2575609a59c22fba9b12b9c
Stephan Jennewein <stephan.jennewein@mailbox.org>
2020-05-08 20:46:27 +0200

    switched map generation to sqlite fixed dates

--- long_lat_to_map_by_species.py
+++ long_lat_to_map_by_species.py
@@ -1,11 +1,13 @@
-import pandas as pd
-from geopy.extra.rate_limiter import RateLimiter
-from geopy.geocoders import Nominatim
 import folium
-import numpy as np
 from folium import plugins
+import sqlite3
+from datetime import datetime
 
-df = pd.read_csv('nabupage_longlat.csv', encoding='utf8')
+try:
+    sqliteConnection = sqlite3.connect('brueter.sqlite')
+    cursor = sqliteConnection.cursor()
+except sqlite3.Error as error:
+    print("Error while connecting to sqlite", error)
 
 map1 = folium.Map(
     location=[52.5163,13.3777],
@@ -16,136 +18,97 @@ folium.TileLayer('cartodbpositron', name='Art').add_to(map1)
 
 marker_cluster = plugins.MarkerCluster(control=False)
 map1.add_child(marker_cluster)
-mauersegler = folium.plugins.FeatureGroupSubGroup(marker_cluster, 'Mauersegler')
-sperling = folium.plugins.FeatureGroupSubGroup(marker_cluster, 'Sperling')
-schwalbe = folium.plugins.FeatureGroupSubGroup(marker_cluster, 'Schwalbe')
-fledermaus = folium.plugins.FeatureGroupSubGroup(marker_cluster, 'Fledermaus')
-star = folium.plugins.FeatureGroupSubGroup(marker_cluster, 'Star')
-andere = folium.plugins.FeatureGroupSubGroup(marker_cluster, 'Andere')
+mauersegler_grp = folium.plugins.FeatureGroupSubGroup(marker_cluster, 'Mauersegler')
+sperling_grp = folium.plugins.FeatureGroupSubGroup(marker_cluster, 'Sperling')
+schwalbe_grp = folium.plugins.FeatureGroupSubGroup(marker_cluster, 'Schwalbe')
+fledermaus_grp = folium.plugins.FeatureGroupSubGroup(marker_cluster, 'Fledermaus')
+star_grp = folium.plugins.FeatureGroupSubGroup(marker_cluster, 'Star')
+andere_grp = folium.plugins.FeatureGroupSubGroup(marker_cluster, 'Andere')
 
-map1.add_child(mauersegler)
-map1.add_child(sperling)
-map1.add_child(schwalbe)
-map1.add_child(fledermaus)
-map1.add_child(star)
-map1.add_child(andere)
+map1.add_child(mauersegler_grp)
+map1.add_child(sperling_grp)
+map1.add_child(schwalbe_grp)
+map1.add_child(fledermaus_grp)
+map1.add_child(star_grp)
+map1.add_child(andere_grp)
 
 positions = []
 popups = []
 colors = []
 url = 'http://www.gebaeudebrueter-in-berlin.de/index.php'
-for index, row in df.iterrows():
-    if row['ID'] in (1214, 1598):
+
+query = ('SELECT gebaeudebrueter.web_id, bezirk, plz, ort, strasse, anhang, erstbeobachtung, beschreibung, besonderes,'
+         'mauersegler, kontrolle, sperling, ersatz, schwalbe, wichtig,'
+         'star, fledermaus, verloren, andere, longitude, latitude from gebaeudebrueter '
+         'LEFT JOIN geolocation_osm ON gebaeudebrueter.web_id = geolocation_osm.web_id')
+cursor.execute(query)
+data = cursor.fetchall()
+for dataset in data:
+    (web_id, bezirk, plz, ort, strasse, anhang, erstbeobachtung, beschreibung, besonderes, mauersegler,
+     kontrolle, sperling, ersatz, schwalbe, wichtig, star, fledermaus, verloren, andere, longitude, latitude) = dataset
+    if web_id in (1214, 1598, 1784):
         continue
     color = 'orange'
     fund = 'andere Art'
-    grp = andere
-    if row['Mauersegler']:
+    grp = andere_grp
+    if mauersegler:
         color='blue'
         fund = 'Mauersegler'
-        grp = mauersegler
-    if row['Sperling']:
+        grp = mauersegler_grp
+    if sperling:
         color='green'
         fund = 'Sperling'
-        grp = sperling
-    if row['Schwalbe']:
+        grp = sperling_grp
+    if schwalbe:
         color='purple'
         fund = 'Schwalbe'
-        grp = schwalbe
-    if row['Fledermaus']:
+        grp = schwalbe_grp
+    if fledermaus:
         color = 'black'
         fund = 'Fledermaus'
-        grp = fledermaus
-    if row['Star']:
+        grp = fledermaus_grp
+    if star:
         color = 'darkred'
         fund = 'Star'
-        grp = star
+        grp = star_grp
     # if row['Andere']:
     #     color = 'orange'
     #     fund = 'andere Art'
     #     grp = andere
     icon = folium.Icon(color=color)
-    if np.isnan(row['latitude']):
+    if latitude == 'None':
         continue
-    if np.isnan(row['longitude']):
+    if longitude == 'None':
         continue
 
-    if row['Ersatz']:
-        ersatz = 'Hier wurden Ersatzmaßnahmen errichtet'
+    if ersatz:
+        ersatz_text = '<br/><br/><b>Hier wurden Ersatzmaßnahmen errichtet</b>'
     else:
-        ersatz = 'Kein Eintrag'
+        ersatz_text = ''
 
-    if row['Sanierung']:
-        sanierung = 'Hier hat eine Sanierung stattgefunden'
-    else:
-        sanierung = 'Kein Eintrag'
+    try:
+        erstbeobachtung = datetime.strptime(erstbeobachtung,'%Y-%m-%d %H:%M:%S')
+        erstbeobachtung_text = erstbeobachtung.strftime('%d.%m.%Y')
+    except:
+        erstbeobachtung_text = 'unbekannt'
 
-    besonderes  = 'Kein Eintrag' if str(row['Besonderes']) == 'nan' else row['Besonderes']
+    besonderes_text  = '<br/><br/><b>Besonderes</b><br/>' + besonderes if besonderes else ''
 
-    popup = folium.Popup('<b>Fund: </b>' + fund + '<br/><br/><b>Adresse</b><br/>' + str(row['Strasse']) + ', ' + str(row['PLZ']) + ' ' + str(row['Ort']) +
-                         '<br/><br/><b>Erstbeobachtung: </b>' + str(row['Erstbeobachtung']) +
-                         '<br/><br/><b>Beschreibung</b><br/>' + str(row['Beschreibung']) +
-                         '<br/><br/><b>Besonderes</b><br/>' + str(besonderes) +
-                         '<br/><br/><b>Ersatz: </b>' + ersatz +
-                         '<br/><br/><b>Sanierung: </b>' + sanierung +
-                         '<br/><br/><b>Link zur Datenbank</b><br/><a href=' + url + '?ID=' + str(row['ID']) + '>' + str(row['ID']) + '</a>'
+    popup = folium.Popup('<b>Fund: </b>' + fund + '<br/><br/><b>Adresse</b><br/>' + str(strasse) + ', ' + str(plz) + ' ' + str(ort) +
+                         '<br/><br/><b>Erstbeobachtung: </b>' + erstbeobachtung_text +
+                         '<br/><br/><b>Beschreibung</b><br/>' + beschreibung +
+                         besonderes_text +
+                         ersatz_text +
+                         '<br/><br/><b>Link zur Datenbank</b><br/><a href=' + url + '?ID=' + str(web_id) + '>' + str(web_id) + '</a>'
                          , max_width=450)
-
-    folium.Marker(location=[row["latitude"], row["longitude"]], popup=popup, tooltip=fund, icon=icon).add_to(grp)
+    try:
+        folium.Marker(location=[latitude, longitude], popup=popup, tooltip=fund, icon=icon).add_to(grp)
+    except:
+        print('bla')
 
 
 folium.LayerControl(collapsed=False, ).add_to(map1)
 
-    # popups.append(popup)
-    # positions.append((row["latitude"], row["longitude"]))
-    # colors.append(color)
-
-
-# plugins.MarkerCluster(positions, popups=popups, color=colors).add_to(map1)
-
-    # folium.Marker(location=[row["latitude"], row["longitude"]],popup=popup,tooltip=fund,icon=icon).add_to(map1)
-
-# df['latitude'] = df['latitude'].replace(np.nan,52.4739285)
-# df['longitude'] = df['longitude'].replace(np.nan,13.4553784)
-# df.apply(lambda row:folium.Marker(location=[row["latitude"], row["longitude"]],
-#                                   popup= folium.Popup('<b>Adresse</b><br/>' + str(row['ADDRESS']) +
-#                                          '<br/><br/><b>Meldung</b><br/>' + str(row['Beschreibung']) +
-#                                          '<br/><br/><b>Datum der Meldung</b><br/>' + str(row['Erstbeobachtung']) +
-#                                          '<br/><br/><b>Besonderes</b><br/>' + str(row['Besonderes']) +
-#                                          '<br/><br/><b>Melder</b><br/>' + str(row['Melder'])
-#                                          ,max_width=450 ),
-#                                   tooltip='Details').add_to(map1), axis=1)
-
-#fg = folium.FeatureGroup(name='fund')
-#map1.add_child(fg)
-
-#g1 = plugins.FeatureGroupSubGroup(fg, 'Mauersegler')
-#map1.add_child(g1)
-
-#g2 = plugins.FeatureGroupSubGroup(fg, 'Sperling')
-#map1.add_child(g2)
-
-#g3 = plugins.FeatureGroupSubGroup(fg, 'Schwalbe')
-#map1.add_child(g3)
-
-#folium.Marker([-1, -1]).add_to(g1)
-#folium.Marker([1, 1]).add_to(g1)
-
-#folium.Marker([-1, 1]).add_to(g2)
-#folium.Marker([1, -1]).add_to(g2)
-
-#folium.LayerControl(collapsed=False).add_to(map1)
-
 map1.save('GebaeudebrueterBerlinBySpecies.html')
-
-
-# print("Latitude = {}, Longitude = {}".format(dflatitude, location.longitude))
-# print(df['latitude'](0))
-# 1 - conveneint function to delay between geocoding calls
-# geocode = RateLimiter(locator.geocode, min_delay_seconds=1)
-# 2- - create location column
-# df['location'] = df['ADDRESS'].apply(geocode)
-# 3 - create longitude, laatitude and altitude from location column (returns tuple)
-# df['point'] = df['location'].apply(lambda loc: tuple(loc.point) if loc else None)
-# 4 - split point column into latitude, longitude and altitude columns
-# df[['latitude', 'longitude', 'altitude']] = pd.DataFrame(df['point'].tolist(), index=df.index)
+if (sqliteConnection):
+    sqliteConnection.close()
