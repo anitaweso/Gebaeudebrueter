366cc51f7f45c264c5c7d09728ccf2df92cf94f5
Anita Wesolowski <anita.wesolowski@gmx.de>
2020-07-09 16:03:11 +0200

    web ID 1598 and 1214 with no address were overwritten data updated

--- long_lat_to_map_by_species.py
+++ long_lat_to_map_by_species.py
@@ -49,7 +49,7 @@ for dataset in data:
     (web_id, bezirk, plz, ort, strasse, anhang, erstbeobachtung, beschreibung, besonderes, mauersegler,
      kontrolle, sperling, ersatz, schwalbe, wichtig, star, fledermaus, verloren, andere, longitude, latitude) = dataset
 
-    if web_id in (1214, 1598, 1784):
+    if web_id == 1784:
         continue
     color = 'orange'
     fund = 'andere Art'
